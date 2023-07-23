ac7b7ff98c0e5e3457b8007bcfc1a70895606255
Moviesgames12345 <70794245+Moviesgames12345@users.noreply.github.com>
2023-07-23 21:50:26 +0930

    Add files via upload

---
+++ Assessment1.py
@@ -0,0 +1,7 @@
+first=int(input("Enter first length "))
+second=int(input("Enter second length "))
+third=int(input("Enter third length "))
+if (first + second) > third:
+    print("Yes, these three lengths can form a triangle")
+else: 
+    print("No, these three lengths cannot form a triangle")
